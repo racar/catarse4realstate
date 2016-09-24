b5d9bc7824b1d9a0298e7a2de0093d18b6a608e2
Darío Fernández Lamattina <dariofl@gmail.com>
2016-09-24 03:17:33 -0300

    config.log_level = :debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Catarse::Application.configure do
   config.force_ssl = false
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
