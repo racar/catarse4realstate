6e39ad0938965c0ce07233a830658cb296dfa3ee
Darío Fernández Lamattina <dariofl@gmail.com>
2016-09-26 21:05:17 -0300

    ASCII 8 BIT - UTF8 Problem

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,10 +1,11 @@
+# -*- encoding : utf-8 -*-
 class Blog
   class << self
     def fetch_last_posts
       Rails.cache.fetch('blog_posts', expires_in: 10.minutes) do
         begin
           feed = Feedjira::Feed.fetch_and_parse("#{CatarseSettings[:blog_url]}?feed=rss2")
-          feed.entries
+          feed.entries.force_encoding("utf8")
         rescue
           []
         end
