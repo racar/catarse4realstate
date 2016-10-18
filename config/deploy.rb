902ab7d1e7f16095712a7300ca90addb6d81ef78
Rafael Carrascal R <rcarrascal@gmail.com>
2016-10-18 12:04:22 -0500

    Fix error seed task

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ namespace :deploy do
   desc "reload the database with seed data"
 
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{fetch :rails_env}"
+    execute "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{fetch :rails_env}"
   end
 
   after :publishing, 'deploy:restart'
