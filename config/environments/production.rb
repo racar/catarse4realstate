8c0e032ed640acd303ba733960eddfa053d4c0ca
Rafael Carrascal R <rcarrascal@gmail.com>
2016-10-26 21:45:04 -0500

    js compress off for debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,9 @@ Catarse::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
-
+  config.assets.debug = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
